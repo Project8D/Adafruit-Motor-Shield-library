c65cf63bbd78741a8c1bcf17fcc4af579865f73b
Project8D <152079856+Project8D@users.noreply.github.com>
2023-11-27 20:23:46 +0500

    Update StepperTest.ino

--- examples/StepperTest/StepperTest.ino
+++ examples/StepperTest/StepperTest.ino
@@ -6,7 +6,7 @@
 
 // Connect a stepper motor with 48 steps per revolution (7.5 degree)
 // to motor port #2 (M3 and M4)
-AF_Stepper motor(48, 2);
+AF_Stepper motor(200, 1);
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
